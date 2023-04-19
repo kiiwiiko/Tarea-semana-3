4718f13f04ec86eac69a8481fdf236ab6155dba1
Ariel <trr.2002j@gmail.com>
2023-04-19 08:32:55 -0500

    progrma listo para ejecutar

--- ejercicio46.c
+++ ejercicio46.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-#include <conio.h>
+
+int factorial(int a)
+{
+    int suma;
+    for (int i = 1; i <= a; i++)
+    {
+        suma = suma + i;
+    }
+    int fac = suma;
+    
+    return fac;
+}
 
 int main(int argc, char const *argv[])
 {
+    int num;
+    printf("Ingresar el numero que desea procesar ");
+    scanf("%d", &num);
+    int resut = factorial(num);
+    int prom = resut / num;
+    printf("El promedio de la suma de los factoriales del numero %d es %d", num, prom);
     return 0;
 }
