89176be2a39789c91630e40fa286ce542997bd77
Ariel <trr.2002j@gmail.com>
2023-04-19 09:07:47 -0500

    progrma listo

--- ejercicio47.c
+++ ejercicio47.c
@@ -3,13 +3,12 @@
 int main(int argc, char const *argv[])
 {
     int suma, num;
-
     printf("Escribir un numero ");
     scanf("%d", &num);
     for (int i = 1; i <= num; i++)
     {
         suma = suma + i;
     }
-    printf("La sumatoria de los factoriales del numero %d es %d", num, suma);
+    printf("La sumatoria de los factoriales del numero %d es igual %d", num, suma);
     return 0;
 }
