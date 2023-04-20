9f4e0449d15322ef2a832700cc66e8fffc3a4585
Ariel <trr.2002j@gmail.com>
2023-04-19 23:27:32 -0500

    solucion

--- ejercicio48.c
+++ ejercicio48.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    int i=0, j=1;
-   do
-   {
-        for (int i = 0; i <= 9; i++)
-    {
-        printf("%d", i);
-    }
-    for (int j = 0; j <= 5; j++)
-    {
-      if (j % 2 == 0) {
-            printf("%d", j);
+#include <stdio.h>
+
+int main() {
+    int x = 0, y = 1;
+    for (int i = 0; i < 10; i++) {
+        if (i % 2 == 0) {
+            printf("%d %d \n", x, y);
+            y++;
         } else {
-            printf("%d", j);
-            j++;
+            printf("%d %d \n", x, y);
+            x++;
+            y++;
         }
     }
-   } while ((i<=9) && (j<=5));
-   return 0;
+    return 0;
 }
---
+++ ejercicio49.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    do
+    {
+        for (int i = 0; i < count; i++)
+        {
+            /* code */
+        }
+        for (size_t i = 0; i < count; i++)
+        {
+            /* code */
+        }
+        for (size_t i = 0; i < count; i++)
+        {
+            /* code */
+        }
+        
+        
+    } while ((i<=9) && (j<=3) && (k<=3));
+    
+
+    return 0;
+}
