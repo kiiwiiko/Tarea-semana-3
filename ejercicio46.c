4f518355ae7247d1b5a22a29d2cc7e233d466e7f
Ariel <trr.2002j@gmail.com>
2023-04-19 22:31:41 -0500

    resolucion para que sume sus factoriales

--- ejercicio46.c
+++ ejercicio46.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 
-int factorial(int a)
-{
-    int suma;
-    for (int i = 1; i <= a; i++)
-    {
-        suma = suma + i;
-    }
-    int fac = suma;
-    
-    return fac;
-}
-
 int main(int argc, char const *argv[])
 {
-    int num;
-    printf("Ingresar el numero que desea procesar ");
+    int suma = 0, num, factorial = 1;
+    printf("Escribir un numero: ");
     scanf("%d", &num);
-    int resut = factorial(num);
-    prom = resut / num;
-    printf("El promedio de la suma de los factoriales del numero %d es %d", num, prom);
+    for (int i = 1; i <= num; i++)
+    {
+        factorial = factorial * i;
+        suma = suma + factorial;
+    }
+    printf("La sumatoria de los factoriales del numero %d es igual a %d", num, suma / num);
     return 0;
 }
